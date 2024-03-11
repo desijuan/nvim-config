14a198cdf62636eeb623f1995eafaf4526936d64
Juan Desimoni <juan.desimoni@codex.com.br>
2024-03-11 00:57:17 -0300

    First commit

---
+++ lua/plugins/autoformat.lua
@@ -0,0 +1,19 @@
+return { -- Autoformat
+  { -- Conform
+    "stevearc/conform.nvim",
+    opts = {
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+      },
+    },
+  },
+
+  { -- Delete Whitespace
+    "mcauley-penney/tidy.nvim",
+    opts = {},
+  },
+}
