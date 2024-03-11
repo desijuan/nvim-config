e66cc3d1733b1238e7cc01e13d913f4385bb439d
Juan Desimoni <juan.desimoni@codex.com.br>
2024-03-11 00:57:17 -0300

    First commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,7 @@
+return {
+  "Mofiqul/vscode.nvim",
+  priority = 1000,
+  init = function()
+    vim.cmd.colorscheme "vscode"
+  end,
+}
---
+++ lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,5 @@
+return { -- Nvim Autopairs
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  opts = {}, -- this is equalent to setup({}) function
+}
